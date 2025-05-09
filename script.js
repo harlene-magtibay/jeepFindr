11add2a200da524836a827d6ea57c2b264cfcd2e
harlene-magtibay <magtibayharlene08@gmail.com>
2025-05-10 03:55:25 +0800

    fetched and displayed the routes on the map for the three jeepney routes

--- script.js
+++ script.js
@@ -15,4 +15,65 @@ const jeepneyStopIcon = L.icon({
   popupAnchor: [1, -34] // point from which the popup should open
 });
 
+// Load and display the GeoJSON route
+fetch('/geojson_files/batangas_alangilan_route.geojson')
+  .then(response => response.json())
+  .then(data => {
+   L.geoJSON(data, {
+  style: {
+    color: 'blue',
+    weight: 8,
+    opacity: 0.7
+  },
+  pointToLayer: function (feature, latlng) {
+  const stopNum = feature.properties.stop_number;
+  const label = L.divIcon({
+    className: 'stop-label',
+    html: `<div style="color:white; background:red; width:18px; border-radius:50%; padding:1px; text-align:center; padding-top:3px; font-size:10px; border:1px solid black;">${stopNum}</div>`,
+    iconSize: [30, 20],
+    iconAnchor: [15, 10]
+  });
+
+  return L.marker(latlng, { icon: label }).bindPopup(`Stop ${stopNum}: ${feature.properties.name || 'Jeepney Stop'}`);
+}
+}).addTo(map);
+  });
+
+
+fetch('/geojson_files/batangas_balagtas_route.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+        filter: function (feature) {
+        return feature.geometry.type !== 'Point';
+      },
+      style: {
+        color: 'red',
+        weight: 5,
+        opacity: 1
+      },
+      pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, { icon: jeepneyStopIcon }).bindPopup(feature.properties.name || 'Jeepney Stop');
+      }
+    }).addTo(map);
+  });
+
+
+fetch('/geojson_files/batangas_capitolio_hospital_route.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+        filter: function (feature) {
+        return feature.geometry.type !== 'Point';
+      },
+      style: {
+        color: 'green',
+        weight: 3,
+        opacity: 1
+      },
+      pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, { icon: jeepneyStopIcon }).bindPopup(feature.properties.name || 'Jeepney Stop');
+      }
+    }).addTo(map);
+  });
 
